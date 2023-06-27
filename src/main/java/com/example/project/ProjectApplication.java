a3333e11fe7c6a3fa593438955e345d603f506e1
insun <kis8294@gmail.com>
2023-06-27 23:25:22 +0900

    구글 로그인

--- src/main/java/com/example/project/ProjectApplication.java
+++ src/main/java/com/example/project/ProjectApplication.java
@@ -2,13 +2,20 @@ package com.example.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ProjectApplication {
-/** 깃허브 테스트용 주석 (편집 시 삭제 바람!!) **/
-	
+
+	@Bean
+	public BCryptPasswordEncoder encoder(){
+		return new BCryptPasswordEncoder();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
+
 
 }
