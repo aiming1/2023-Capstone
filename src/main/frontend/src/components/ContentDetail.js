3f270f294e40545e0d6deaf7b037dc5b8c6981d7
Kim SeulA <seulah0629@gmail.com>
2023-09-06 20:49:07 +0900

    상품 사진 여러 개 띄우기

--- src/main/frontend/src/components/ContentDetail.js
+++ src/main/frontend/src/components/ContentDetail.js
@@ -172,11 +172,12 @@ const ContentDetail = (props) => {
 
       <div className={styles.line}></div>
       <div className={styles.div8}>{data?.details}</div>
-{/*      <div className={styles.div9}>
+
+/*      <div className={styles.div9}>
         <p className={styles.p}>거래거래</p>
         <p className={styles.p}>대충 거래글 끝</p>
       </div>
-      
+
       <div className={styles.group}>
         <ImageSlide images={data?.image} />
         <div className={styles.div12}>
